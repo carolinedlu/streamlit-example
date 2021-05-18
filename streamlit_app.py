d2d3135b0375e68592c60e0b1fc013936e3c3385
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-05-18 15:50:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import base64
 
-filename=open('example.xlsx', errors='ignore')
+filename=open('example.xlsx')
 href = f'<a href="data:file/xlsx" download="file.xlsx">Download XLSX File</a>'
 st.markdown(href, unsafe_allow_html=True)
