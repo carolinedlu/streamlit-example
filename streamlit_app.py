0856eacfc61949e0e07709d5ad4e35cac456c99f
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-05-18 16:36:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import pandas as pd
 import base64
 import io
 
-b64 = base64.b64encode(data).decode('UTF-8')
-href = f'<a href="data:file/xlsx;base64,{b64}" download="example.xlsx">Download xlsx File</a>'
-st.markdown(href, unsafe_allow_html=True)
+vals= ['A','B','C']
+df= pd.DataFrame(vals, columns=["Title"])
+
+towrite = io.BytesIO()
+downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
+towrite.seek(0)  # reset pointer
+b64 = base64.b64encode(towrite.read()).decode()  # some strings
+linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="myfilename.xlsx">Download excel file</a>'
+st.markdown(linko, unsafe_allow_html=True)
