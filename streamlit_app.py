ada78537919f2b6557f0e7c243b362b8fdd821e2
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-05-18 16:29:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,6 @@ import pandas as pd
 import base64
 import io
 
-df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
-
-excel_path = 'test.xlsx'
-xlsx = df.to_excel(excel_path)
-
-data = open(excel_path, 'rb').read()
 b64 = base64.b64encode(data).decode('UTF-8')
-href = f'<a href="data:file/xlsx;base64,{b64}" download="Example.xlsx">Download xlsx File</a>'
-st.markdown(linko, unsafe_allow_html=True)
+href = f'<a href="data:file/xlsx;base64,{b64}" download="example.xlsx">Download xlsx File</a>'
+st.markdown(href, unsafe_allow_html=True)
